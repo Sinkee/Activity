71514f159ed50f0306f32d7de20415bad760a052
SunCode <chenxiaoyangal@163.com>
2015-06-10 13:38:58 +0800

    Initialize Web/DB

---
+++ ActivityProject/ActivityDB/Users.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Users]
+(
+	[Id] INT NOT NULL PRIMARY KEY identity(100000,1),
+	UserName nvarchar(100),
+	NickName nvarchar(100),
+	IsDelete bit ,
+	IsLocked Bit,
+	CreateTime datetime
+)
